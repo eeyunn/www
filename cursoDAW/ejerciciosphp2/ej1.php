6d0057afbde35f1390d4bde2865eab880f70080c
pepicooo <jose.illan.lopez@ces-vegamedia.es>
2021-10-08 11:45:40 +0200

    organisao

--- cursoDAW/ejerciciosphp2/ej1.php
+++ cursoDAW/ejerciciosphp2/ej1.php
@@ -5,15 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php
+        function prueba(){
+            print("Pa casa");
+        }
+        include("funciones1.php");
+    ?>
 </head>
 <body>
     <?php
-        echo "Escribe una frase: <br>";
-        
+        prueba();
     ?>
-    <form method = "post" action = "pagina.php">
-    &iquest;Cu&aacute;l es el título de tu pel&iacute;cula favorita?
-    <input name = "pelicula" type = "text" max length = "10" size = "15">
-    </form>
 </body>
 </html>
---
+++ cursoDAW/ejerciciosphp2/funciones1.php
@@ -0,0 +1,9 @@
+<?php
+function prueba2(){
+    print("Esto es otro texto, en fin, otro rollo");
+}
+
+function pruebareturn($numero){
+    return $numero;
+}
+?>
