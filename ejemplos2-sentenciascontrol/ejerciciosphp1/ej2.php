41752f13910a12df8d829494e0f3710f91031fee
pepicooo <jose.illan.lopez@ces-vegamedia.es>
2021-10-08 08:43:00 +0200

    pum

---
+++ ejemplos2-sentenciascontrol/ejerciciosphp1/ej2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Escribe una frase: <br>";
+        
+    ?>
+    <form method = "post" action = "pagina.php">
+    &iquest;Cu&aacute;l es el título de tu pel&iacute;cula favorita?
+    <input name = "pelicula" type = "text" max length = "10" size = "15">
+    </form>
+</body>
+</html>
