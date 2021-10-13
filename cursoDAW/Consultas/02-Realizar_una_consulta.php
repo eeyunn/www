0fb50956adadc7121ee9275ab07b9e438d37badd
pepicooo <jose.illan.lopez@ces-vegamedia.es>
2021-10-13 13:41:38 +0200

    pum

--- cursoDAW/Consultas/02-Realizar_una_consulta.php
+++ cursoDAW/Consultas/02-Realizar_una_consulta.php
@@ -6,47 +6,52 @@
 </head>
 
 <body>
-	<?php
-	//creamos la conexion
-	$conn = mysqli_connect('localHost', 'root', '');
-	//Imprimos el error si se ha producido
-	echo mysqli_error($conn);
+	<table>
+		<tr>
+			<th>Nombre</th>
+			<th>Apellidos</th>
+			<th>Provincia</th>
+			<th>Edad</th>
+			<th>Profesión</th>
+			<th>Sexo</th>
+		</tr>
+
+		<?php
+		//creamos la conexion
+		$conn = mysqli_connect('localHost', 'root', '');
+		//Imprimos el error si se ha producido
+		echo mysqli_error($conn);
 
 
-	//Seleccionamos la base de datos
-	mysqli_select_db($conn, 'pruebas');
-	//Imprimos el error si se ha producido. mysql_error siempre va a mostrar el error de la última función mysql ejecutada
-	echo mysqli_error($conn);
-
-
-	//Me creo la consulta que voy a ejecutar. En este caso voy a obtener todas las personas de la base de datos
-	$consulta = "SELECT * FROM PERSONAS";
-
-	$result = mysqli_query($conn, $consulta);
-	//Imprimos el error si se ha producido. mysql_error siempre va a mostrar el error de la última función mysql ejecutada
-	echo mysqli_error($conn);
-
-
-
-	while ($row = mysqli_fetch_array($result)) {
-		print("Forma1: " . $row[0] . " || " . $row[1]);
-		print("<br>");
-
-		print("Forma2: " . $row['NOMBRE'] . " || " . $row['APELLIDOS']); //ESTA MEJOR, SE ENTIENDE NO?
-		print("<br>");
-
-		print("------------------------------");
-		print("<br>");
-
+		//Seleccionamos la base de datos
+		mysqli_select_db($conn, 'pruebas');
 		//Imprimos el error si se ha producido. mysql_error siempre va a mostrar el error de la última función mysql ejecutada
-		mysqli_error($conn);
-	}
-
-	mysqli_close($conn);
+		echo mysqli_error($conn);
 
 
-	?>
+		//Me creo la consulta que voy a ejecutar. En este caso voy a obtener todas las personas de la base de datos
+		$consulta = "SELECT * FROM PERSONAS";
 
+		$result = mysqli_query($conn, $consulta);
+		//Imprimos el error si se ha producido. mysql_error siempre va a mostrar el error de la última función mysql ejecutada
+		echo mysqli_error($conn);
+
+
+
+		while ($row = mysqli_fetch_array($result)) {
+			print("<tr>"."<td>".$row['NOMBRE']."</td>"."<td>".$row['APELLIDOS']."</td>".
+			"<td>".$row['PROVINCIA']."</td>"."<td>".$row['EDAD']."</td>".
+			"<td>".$row['PROFESION']."</td>"."<td>".$row['SEXO']."</td>"."</tr>");
+
+			//Imprimos el error si se ha producido. mysql_error siempre va a mostrar el error de la última función mysql ejecutada
+			mysqli_error($conn);
+		}
+
+		mysqli_close($conn);
+
+
+		?>
+	</table>
 </body>
 
 </html>
